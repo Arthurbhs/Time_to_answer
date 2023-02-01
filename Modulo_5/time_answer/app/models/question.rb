023ca446f4390474323ab794668e5279274b07c9
arthur <arthur.shekinarfoxy@gmail.com>
2023-02-01 09:57:17 -0300

    feat: using  Cocoon

--- Modulo_5/time_answer/app/models/question.rb
+++ Modulo_5/time_answer/app/models/question.rb
@@ -1,4 +1,5 @@
 class Question < ApplicationRecord
- has_many :questions
+ belongs_to :subject, inverse_of: :questions
  has_many :answers
+ accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
 end
