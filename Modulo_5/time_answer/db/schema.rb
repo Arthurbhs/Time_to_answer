e22a59419dc83c46d84388099e96e24aa2cc3207
arthur <arthur.shekinarfoxy@gmail.com>
2023-02-02 12:59:42 -0300

    feat: user perfil

--- Modulo_5/time_answer/db/schema.rb
+++ Modulo_5/time_answer/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_31_131144) do
+ActiveRecord::Schema.define(version: 2023_02_02_151229) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -55,6 +55,8 @@ ActiveRecord::Schema.define(version: 2023_01_31_131144) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
