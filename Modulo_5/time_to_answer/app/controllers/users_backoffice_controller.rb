8c7df561e19c127c9897f6f345a4c9b3f84a0096
arthur <arthur.shekinarfoxy@gmail.com>
2023-02-03 12:54:18 -0300

    Feat: shortcut to routes

--- Modulo_5/time_to_answer/app/controllers/users_backoffice_controller.rb
+++ Modulo_5/time_to_answer/app/controllers/users_backoffice_controller.rb
@@ -5,7 +5,8 @@ class UsersBackofficeController < ApplicationController
 
   private
 
+ 
   def build_profile
-    current_user.build_user_profile if current_user.user_profile.blank?
-  end
+    current_user.build_user_profile   if current_user.user_profile.blank?
+end
 end
